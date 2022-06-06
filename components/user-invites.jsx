587e903296acc483103c4f320e16e107831c6fa4
Wendell Encorporado <brodwen83@gmail.com>
2022-06-06 08:08:17 +0800

    export datatable as csv excel or pdf

--- components/user-invites.jsx
+++ components/user-invites.jsx
@@ -17,6 +17,7 @@ export default function UserInvites({
   isLoading = false,
   refetchInvites,
 }) {
+  const dt = useRef(null);
   const { crmAPIText } = useUserContext();
   const [selectedInvitee, setSelectedInvitee] = useState(null);
   const [showInviteeDetailsModal, setShowInviteeDetailsModal] = useState(false);
@@ -76,6 +77,214 @@ export default function UserInvites({
     }
   };
 
+  // <Column field="first_name" sortable header="First Name"></Column>
+  //         <Column field="last_name" sortable header="Last Name"></Column>
+  //         <Column field="email" sortable header="Email"></Column>
+  //         <Column field="company" sortable header="Company"></Column>
+  //         <Column field="position" sortable header="Position"></Column>
+  //         <Column field="country" sortable header="Country"></Column>
+
+  const cols = [
+    { field: "first_name", header: "First Name" },
+    { field: "last_name", header: "Last Name" },
+    { field: "email", header: "Email" },
+    { field: "company", header: "Company" },
+    { field: "position", header: "Position" },
+    { field: "country", header: "Country" },
+  ];
+
+  const exportColumns = cols.map((col) => ({
+    title: col.header,
+    dataKey: col.field,
+  }));
+
+  // const importCSV = (e) => {
+  //   const file = e.files[0];
+  //   const reader = new FileReader();
+  //   reader.onload = (e) => {
+  //     const csv = e.target.result;
+  //     const data = csv.split("\n");
+
+  //     // Prepare DataTable
+  //     const cols = data[0].replace(/['"]+/g, "").split(",");
+  //     data.shift();
+
+  //     let _importedCols = cols.map((col) => ({
+  //       field: col,
+  //       header: toCapitalize(col.replace(/['"]+/g, "")),
+  //     }));
+  //     let _importedData = data.map((d) => {
+  //       d = d.split(",");
+  //       return cols.reduce((obj, c, i) => {
+  //         obj[c] = d[i].replace(/['"]+/g, "");
+  //         return obj;
+  //       }, {});
+  //     });
+
+  //     setImportedCols(_importedCols);
+  //     setImportedData(_importedData);
+  //   };
+
+  //   reader.readAsText(file, "UTF-8");
+  // };
+
+  // const importExcel = (e) => {
+  //   const file = e.files[0];
+
+  //   import("xlsx").then((xlsx) => {
+  //     const reader = new FileReader();
+  //     reader.onload = (e) => {
+  //       const wb = xlsx.read(e.target.result, { type: "array" });
+  //       const wsname = wb.SheetNames[0];
+  //       const ws = wb.Sheets[wsname];
+  //       const data = xlsx.utils.sheet_to_json(ws, { header: 1 });
+
+  //       // Prepare DataTable
+  //       const cols = data[0];
+  //       data.shift();
+
+  //       let _importedCols = cols.map((col) => ({
+  //         field: col,
+  //         header: toCapitalize(col),
+  //       }));
+  //       let _importedData = data.map((d) => {
+  //         return cols.reduce((obj, c, i) => {
+  //           obj[c] = d[i];
+  //           return obj;
+  //         }, {});
+  //       });
+
+  //       setImportedCols(_importedCols);
+  //       setImportedData(_importedData);
+  //     };
+
+  //     reader.readAsArrayBuffer(file);
+  //   });
+  // };
+
+  const exportCSV = (selectionOnly) => {
+    dt.current.exportCSV({ selectionOnly });
+  };
+
+  const exportPdf = () => {
+    import("jspdf").then((jsPDF) => {
+      import("jspdf-autotable").then(() => {
+        const doc = new jsPDF.default(0, 0);
+        doc.autoTable(exportColumns, invites);
+        doc.save("products.pdf");
+      });
+    });
+  };
+
+  const exportExcel = () => {
+    import("xlsx").then((xlsx) => {
+      const worksheet = xlsx.utils.json_to_sheet(invites);
+      const workbook = { Sheets: { data: worksheet }, SheetNames: ["data"] };
+      const excelBuffer = xlsx.write(workbook, {
+        bookType: "xlsx",
+        type: "array",
+      });
+      saveAsExcelFile(excelBuffer, "mmt-invites");
+    });
+  };
+
+  const saveAsExcelFile = (buffer, fileName) => {
+    import("file-saver").then((module) => {
+      if (module && module.default) {
+        let EXCEL_TYPE =
+          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+        let EXCEL_EXTENSION = ".xlsx";
+        const data = new Blob([buffer], {
+          type: EXCEL_TYPE,
+        });
+
+        module.default.saveAs(
+          data,
+          fileName + "_export_" + new Date().getTime() + EXCEL_EXTENSION
+        );
+      }
+    });
+  };
+
+  // const toCapitalize = (s) => {
+  //   return s.charAt(0).toUpperCase() + s.slice(1);
+  // };
+
+  // const clear = () => {
+  //   setImportedData([]);
+  //   setSelectedImportedData([]);
+  //   setImportedCols([{ field: "", header: "Header" }]);
+  // };
+
+  // const onImportSelectionChange = (e) => {
+  //   setSelectedImportedData(e.value);
+  //   const detail = e.value.map((d) => Object.values(d)[0]).join(", ");
+  //   toast.current.show({
+  //     severity: "info",
+  //     summary: "Data Selected",
+  //     detail,
+  //     life: 3000,
+  //   });
+  // };
+
+  // const onSelectionChange = (e) => {
+  //   setSelectedProducts(e.value);
+  // };
+
+  const header = (
+    <div className="flex align-items-center export-buttons">
+      <Button
+        type="button"
+        icon="pi pi-file"
+        // onClick={() => alert("export csv")}
+        onClick={() => exportCSV(false)}
+        className="mr-2"
+        data-pr-tooltip="CSV"
+        tooltip="Export CSV"
+        tooltipOptions={{ position: "top" }}
+      />
+      <Button
+        type="button"
+        icon="pi pi-file-excel"
+        // onClick={() => alert("export excel")}
+        onClick={exportExcel}
+        className="p-button-success mr-2"
+        data-pr-tooltip="XLS"
+        tooltip="Export Excel"
+        tooltipOptions={{ position: "top" }}
+      />
+      <Button
+        type="button"
+        icon="pi pi-file-pdf"
+        // onClick={() => alert("export pdf")}
+        onClick={exportPdf}
+        className="p-button-warning mr-2"
+        data-pr-tooltip="PDF"
+        tooltip="Export PDF"
+        tooltipOptions={{ position: "top" }}
+      />
+      {/* <Button
+        type="button"
+        icon="pi pi-filter"
+        // onClick={() => alert("export csv")}
+        // onClick={() => exportCSV(true)}
+        className="p-button-info ml-auto"
+        data-pr-tooltip="Selection Only"
+      /> */}
+      <Button
+        type="button"
+        disabled={isLoading}
+        loading={isLoading}
+        icon="pi pi-refresh"
+        // label="Refresh"
+        onClick={() => refetchInvites()}
+        className="p-button-info ml-auto"
+        tooltip="Refresh List"
+        tooltipOptions={{ position: "top" }}
+      />
+    </div>
+  );
+
   return (
     <>
       <Dialog
@@ -105,15 +314,10 @@ export default function UserInvites({
             />
             <label htmlFor="acceptance"> Only Show Accepted Invites</label>
           </div>
-          <Button
-            disabled={isLoading}
-            loading={isLoading}
-            icon="pi pi-refresh"
-            // label="Refresh"
-            onClick={() => refetchInvites()}
-          />
         </div>
+
         <DataTable
+          ref={dt}
           value={invites}
           stripedRows
           selectionMode="single"
@@ -124,6 +328,7 @@ export default function UserInvites({
           onRowSelect={onRowSelect}
           emptyMessage="No Invites..."
           loading={isLoading}
+          header={header}
         >
           <Column field="first_name" sortable header="First Name"></Column>
           <Column field="last_name" sortable header="Last Name"></Column>
