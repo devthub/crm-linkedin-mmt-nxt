3aadb8f6583108143521cca23fbb3d587f3c589a
Wendell Encorporado <brodwen83@gmail.com>
2023-03-08 10:08:29 +0800

    fix: cors headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/api/auth/:path*",
+        has: [
+          // eslint-disable-next-line no-useless-escape
+          {
+            type: "header",
+            key: "Origin",
+            value: "(?<origin>^https://.*.crm-hub.tech$)",
+          },
+        ],
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: ":origin",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = {
